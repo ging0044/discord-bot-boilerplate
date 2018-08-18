7f3e99ae1a4dd8ef68c57dedd0b6eaa65a96e893
Patrick Gingras <ging0044@algonquinlive.com>
2018-08-18 17:59:17 -0400

    i18n module

---
+++ src/Module/i18n.js
@@ -0,0 +1,37 @@
+const Module = require(".");
+
+class I18n extends Module {
+  constructor() {
+    super();
+
+    this.root = this.discord.registerCommand(
+      "i18n",
+      () => {
+        return "some stats";
+      },
+      {
+        description: this.i.getMessage("en", "module/i18n/description")
+      }
+    );
+  }
+
+  execute() {
+    this.root.registerSubcommand(
+      "reload",
+      () => {
+        this.i.reloadMessages();
+        return this.i.getMessage("en", "module/i18n/reload/reloadedMessages");
+      },
+      {
+        description: this.i.getMessage("en", "module/i18n/reload/description"),
+        requirements: {
+          userIDs: [
+            "122351209486090240",
+          ]
+        }
+      }
+    );
+  }
+}
+
+module.exports = I18n;
