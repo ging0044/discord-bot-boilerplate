6a5532ccf7824ef224f6575ce2aa5cc9612d86a5
Patrick Gingras <ging0044@algonquinlive.com>
2018-08-18 17:58:58 -0400

    implemented message files (for translation)

--- src/Lib/Internationalization/index.js
+++ src/Lib/Internationalization/index.js
@@ -1,46 +1,74 @@
 const Lib = require("..");
 const cldr_data = require("cldr-data");
+const path = require("path");
 const Globalize = require("globalize");
-// TODO: load message files too
+
+const MESSAGE_PATH = path.resolve("i18n/", "messages");
 
 class Internationalization extends Lib {
   constructor() {
     super();
 
-    this.loadI18nData()
-      .then(() =>
-        this.getLogger().info("Loaded i18n stuff"))
-      .catch((e) =>
-        this.getLogger().error(`Failed to load i18n stuff: ${e}\n${e.stack}`));
+    Globalize.load(cldr_data.entireSupplemental());
+    Globalize.loadMessages(require(MESSAGE_PATH));
+
+    this._instances = {};
+
+    this.getMessage = this.getMessage.bind(this);
   }
 
-  execute() {
-    return Internationalization;
+  static newInstance(locale) {
+    Globalize.load(cldr_data.entireMainFor(locale));
+    return new Globalize(locale);
   }
 
-  async loadI18nData() {
-    return await new Promise((resolve, reject) => {
-      try {
-        process.stdout.write("Loading all the locale stuff into ram...\n");
-        Globalize.load(cldr_data.all());
-
-        Internationalization.i = cldr_data.availableLocales.reduce((acc, loc) => {
-          acc[loc] = new Globalize(loc);
-          return acc;
-        }, {});
-        resolve();
-      }
-      catch (e) {
-        reject(e);
-      }
-    });
+  _getLocaleInstance(locale) {
+    if (this._instances[locale]) {
+      return this._instances[locale];
+    }
+    this._instances[locale] = Internationalization.newInstance(locale);
+    return this._instances[locale];
   }
 
-  static m(loc, message) {
-    return `${message} in ${loc}`; // TODO: get message, if missing, add to files somehow
+  getMessage(locale, path, ...args) {
+    const instance = this._getLocaleInstance(locale);
+
+    try {
+      return instance.formatMessage(path, ...args);
+    }
+    catch(_) {
+      this.getLogger().warn(
+        `No message "${path}" found for locale "${locale}"`
+      );
+      this.addMessage(locale, path)
+        .catch(error =>
+          this.getLogger().error(
+            `Unable to add message for ${locale}: ${path}\n${
+              error.stack
+            }`
+          ));
+
+      return `No translation available for ${path} in ${locale}`;
+    }
+  }
+
+  addMessage(locale, path) {
+    return Promise.resolve();
+  }
+
+  reloadMessages() {
+    delete require.cache[require.resolve(MESSAGE_PATH)];
+
+    this.getLogger().info("Reloading message file");
+    Globalize.loadMessages(require(MESSAGE_PATH));
   }
 
   // TODO: add wrappers around a bunch of i18n stuff (pluralize, dates, whatever)
+
+  execute() {
+    this.getLogger().info("Ready");
+    return this;
+  }
 }
 
 module.exports = Internationalization;
