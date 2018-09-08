30f08067a680b2df5d8da9da6fc052d41661fdcd
Patrick Gingras <ging0044@algonquinlive.com>
2018-09-08 14:16:13 -0400

    reorder dep config so logger is loaded first

--- config/example/dependencies.config.js
+++ config/example/dependencies.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   dependencies: [
+    { name: "logger", path: "src/Lib/Logger" },
     { name: "db", path: "src/Lib/DB" },
     { name: "discord", path: "src/Lib/Discord" },
     { name: "internationalization", path: "src/Lib/Internationalization" },
-    { name: "logger", path: "src/Lib/Logger" },
   ],
   autoRequire: [
     "src/Module",
