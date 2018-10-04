cd3b0c0c294088279c4a3afae48dff6146568491
Patrick Gingras <ging0044@algonquinlive.com>
2018-10-04 19:56:42 -0400

    add Command to dependency config example

--- config/example/dependencies.config.js
+++ config/example/dependencies.config.js
@@ -2,6 +2,7 @@ module.exports = {
   dependencies: [
     { name: "logger", path: "src/Lib/Logger" },
     { name: "db", path: "src/Lib/DB" },
+    { name: "command", path: "src/Lib/Command" },
     { name: "discord", path: "src/Lib/Discord" },
     { name: "internationalization", path: "src/Lib/Internationalization" },
   ],
