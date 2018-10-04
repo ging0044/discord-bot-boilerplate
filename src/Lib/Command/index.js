7a6edb19f0e54c4e96d4a7d4c6f8785f14c252ea
Patrick Gingras <ging0044@algonquinlive.com>
2018-10-04 19:54:18 -0400

    move command into its own directory

--- src/Lib/Command/index.js
+++ src/Lib/Command/index.js
@@ -16,21 +16,21 @@ class Command {
     this._commands[name] = handler;
   }
 
-  execute(argv) {
-    Command.execute(this._commands, argv);
+  execute(argv, ...params) {
+    Command.execute(this._commands, argv, ...params);
   }
 
-  static execute(handlers, argv) {
+  static execute(handlers, argv, ...params) {
     let handler = handlers[argv._[0]];
     let args = argv;
 
     for (;;) {
       args = shift(args);
       if (handler instanceof Command) {
-        return handler.execute(args);
+        return handler.execute(args, ...params);
       }
       else if (typeof handler === "function") {
-        return handler(args._, args);
+        return handler(args._, args, ...params);
       }
       else if (typeof handler === "object") {
         handler = handler[args._[0]];
