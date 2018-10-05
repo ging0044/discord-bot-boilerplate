141214f1b39d8e5c052d541e64ebd8603fd505df
Patrick Gingras <ging0044@algonquinlive.com>
2018-10-04 20:00:45 -0400

    Improved command implementation, should be good like this for now

--- src/Lib/Command/index.js
+++ src/Lib/Command/index.js
@@ -7,20 +7,51 @@ function shift(argv) {
   );
 }
 
+function id(a) {
+  return a;
+}
+
 class Command {
-  constructor(commands = {}) {
+  constructor(commands = {}, opts = {}) {
+    const { description = "", usage = "" } = opts;
     this._commands = commands;
+    this._description = description;
+    this._usage = usage;
   }
 
   addCommand(name, handler) {
     this._commands[name] = handler;
+    return this;
   }
 
-  execute(argv, ...params) {
-    Command.execute(this._commands, argv, ...params);
+  setCommands(commands) {
+    this._commands = commands;
+    return this;
   }
 
-  static execute(handlers, argv, ...params) {
+  setDescription(description) {
+    this._description = description;
+    return this;
+  }
+
+  getDescription() {
+    return this._description;
+  }
+
+  setUsageText(text) {
+    this._usage = text;
+    return this;
+  }
+
+  getUsageText() {
+    return this._usage;
+  }
+
+  evaluate(argv, ...params) {
+    return Command.evaluate(this._commands, argv, ...params);
+  }
+
+  static evaluate(handlers, argv, ...params) {
     let handler = handlers[argv._[0]];
     let args = argv;
 
@@ -39,6 +70,14 @@ class Command {
         throw new Error(`Unknown command ${args._[0]}`);
       }
     }
+  }
+
+  static getHelpText(translator = id) {
+
+  }
+
+  execute() {
+    return Command;
   }
 }
 
